9b32a22837d77076ba93c93d906068383480b835
vrndrmry <117343379+vrndrmry@users.noreply.github.com>
2024-02-28 18:31:46 +0530

    updating the post and getting all post completed

--- controllers/blog.js
+++ controllers/blog.js
@@ -2,8 +2,8 @@ import jwt from "jsonwebtoken";
 import postModel from "../models/Blog.js";
 import User from "../models/User.js";
 
-export const getAllPost = (req, res) => {
-  res.send("Getting all post");
+export const getAllPost = async (req, res) => {
+  res.json(await postModel.find().sort({ createdAt: -1 }));
 };
 
 // Creating a post
@@ -11,6 +11,7 @@ export const createNewPost = async (req, res) => {
   try {
     const token = req.cookies.token;
     const id = req.params.id;
+
     if (token) {
       jwt.verify(token, process.env.SECRET_KEY, async (err, info) => {
         if (err) res.status(401).json({ message: "Cookies not verified" });
@@ -58,7 +59,67 @@ export const deletePost = async (req, res) => {
       res.status(200).json({ message: "Post deleted" });
     }
   } catch (err) {
-    console.log(err);
+    res.json(err);
+  }
+};
+
+export const updatePost = async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    const { id, postid } = req.params;
+    if (token) {
+      jwt.verify(token, process.env.SECRET_KEY, async (err, info) => {
+        if (err) res.status(401).json({ message: "Cookies not verified" });
+
+        let newImage = null; //Create a variable to initialize the image so that if user don't want to update the image
+        const { typeOfPost, title, summary, content } = req.body;
+
+        const updatePostDoc = await postModel
+          .findById(postid)
+          .catch((err) => new Error("invalid post update request")); // handling the error is the postid is not valid
+
+        const isAuthor =
+          JSON.stringify(updatePostDoc.authorId) === JSON.stringify(id);
+
+        const isPost =
+          JSON.stringify(updatePostDoc._id) === JSON.stringify(postid);
+
+        if (!isAuthor) {
+          return res
+            .status(400)
+            .json({ message: "You are not actual user" });
+        }
+
+        if (!isPost) {
+          return res
+            .status(400)
+            .json({ message: "You are acessing invalid post" });
+        }
+
+        if (req.file) {
+          newImage = {
+            name: req.file.originalname,
+            filename: req.file.filename,
+            data: req.file.buffer,
+            contentType: req.file.mimeType,
+            path: req.file.path,
+          };
+        }
+
+        const updatedDocument = await postModel.updateOne({
+          typeOfPost,
+          title,
+          summary,
+          content,
+          author: info.name,
+          authorId: info.id,
+          image: newImage ? newImage : updatePostDoc.image,
+        });
+
+        res.status(200).json({message:"Post Update"});
+      });
+    }
+  } catch (err) {
     res.json(err);
   }
 };
--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import {createNewPost, deletePost, getAllPost} from '../controllers/blog.js'
+import {createNewPost, deletePost, getAllPost, updatePost} from '../controllers/blog.js'
 import blogUpload from '../middleware/blog.js'
 const router = express.Router()
 
 router.get('/post',getAllPost)
 router.post(`/:id/create`,blogUpload.single('image'),createNewPost)
 router.delete(`/:id/delete/:postid`,deletePost)
+router.put("/:id/update/:postid", blogUpload.single("image"), updatePost);
 export default router
