911c9747b71849e2a2cb8ba1368eb4070ffe9500
vrndrmry <117343379+vrndrmry@users.noreply.github.com>
2024-04-09 15:32:21 +0530

    imported http module in index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import https from "https";
 import fs from 'fs'
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -30,22 +31,15 @@ const connect = async () => {
 };
 
 // Middlewares
+
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.json());
-app.use(cors());
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(cors({ origin: "*" }));
-
 app.use((req, res, next) => {
-  // Allow requests from any origin
   res.setHeader("Access-Control-Allow-Origin", "*");
-  // Allow certain methods
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  // Allow certain headers
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  // Allow credentials (if required)
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
@@ -62,11 +56,11 @@ const server = https.createServer(httpsOptions, app);
 app.use("/api/contact/uploads/contactus", express.static("uploads"));
 
 app.use("/api/contact", contactusRoute);
-app.use("/api/login", loginForm);
-app.use("/api", post);
 app.use("/api/proposal/website",websiteProposalRoute)
 app.use("/api/proposal/software",softwareProposalRoute)
 app.use('/api/proposal/application',applicationProposalRoute)
+app.use("/api/login", loginForm);
+app.use("/api", post);
 
 
 // Listening to the post
